54c33405393f9932e9da9ec1f0ee3e43df7fe03f
Gwyn Jones <m@gwnjns.com>
2019-07-09 20:03:52 +0100

    Add variables

---
+++ README.md
@@ -0,0 +1,5 @@
+# Boys
+
+Teaching my kids to code
+
+* [variables](/variables.js)
---
+++ variables.js
@@ -0,0 +1,15 @@
+process.stdout.write('\033c');
+
+var age = 44;
+
+new_age = age + 4;
+
+console.log(new_age);
+
+var name = "Zac";
+
+console.log(name);
+
+var full_name = name + " the Genius";
+
+console.log(full_name);
