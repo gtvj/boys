19b82e2844988819ad53ad71b2588679e4428ce1
Gwyn Jones <m@gwnjns.com>
2019-07-11 21:04:00 +0100

    Add while loops

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Code adventures in the Jones family
 
 * [variables](/variables.js)
 * [conditionals with `if()`](/conditionals_if.js)
+* [loops - while](/loops_while.js)
---
+++ loops_while.js
@@ -0,0 +1,8 @@
+age = 6;
+
+while(age < 12) {
+    console.log(`Sorry, you still can't see a 12 rated film because you are ` + age);
+    age = age + 1;
+}
+
+console.log('OK, you can watch that 12 rated film now.');
